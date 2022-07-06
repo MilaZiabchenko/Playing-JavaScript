6139ad66ea97e01095b0ac13e2d5fe50972b0ad4
Mila Ziabchenko <mi.podgurska@gmail.com>
2022-07-06 20:15:05 +0300

    Refactors playingesnext.js

--- Modern JavaScript/js/playingesnext.js
+++ Modern JavaScript/js/playingesnext.js
@@ -2,37 +2,42 @@
 
 // Sets are collections of values that can be of any type, but the rule with Sets is that each value must be unique
 
-let books = new Set();
+const array = [2, 3, 4, 2, 4, '8', 8, '8', [8, 8]];
 
-books
+const arrayOfUniqueValues = [...new Set(array)];
+const setOfUniqueValues = new Set(array);
+
+console.log(arrayOfUniqueValues);
+console.log(setOfUniqueValues);
+
+console.log(arrayOfUniqueValues.length === setOfUniqueValues.size);
+
+const setOfBooks = new Set();
+
+setOfBooks
   .add('Pride and Prejudice')
   .add('Pride and Prejudice')
   .add('War and Peace')
   .add('Oliver Twist');
 
-console.log(books);
-console.log(books.size);
-console.log(`books include 'Oliver Twist': ${books.has('Oliver Twist')}`);
+console.log(setOfBooks);
+console.log(setOfBooks.size);
+console.log(setOfBooks.has('Oliver Twist'));
 
-books.delete('War and Peace');
+setOfBooks.delete('War and Peace');
 
-console.log(books);
+console.log(setOfBooks);
 
-// We can iterate through the elements of a Set in insertion order
-books.forEach(book => console.log(book));
+// We can iterate through the elements of a Set in the insertion order
+setOfBooks.forEach(book => console.log(book));
 
-const getArrayWithUniqueElements = array => [...new Set(array)];
-
-const array = [2, 3, 4, 2, 4, '8', 8, '8', [8, 8]];
-
-console.log(new Set(array));
-console.log(getArrayWithUniqueElements(array));
+for (let book of setOfBooks) console.log(book);
 
 // Map data structure
 
 // The Map object holds 'key - value' pairs. In a Map, any value, both objects and primitive values may be used either as a key or a value
 
-let details = new Map([
+const details = new Map([
   [new Date(), 'today'],
   [{ javascript: ['JS', 'Node', 'React'] }, '82 hours'],
   [
@@ -45,35 +50,33 @@ details.set('students', 12);
 console.log(details);
 console.log(details.size);
 
-// Unlike objects, Maps can iterate their elements in their insertion order
-details.forEach(item => console.log(item));
-
-let course = new Map();
+const course = new Map();
 
 course
   .set('React', { description: 'UI Library' })
   .set('Jest', { description: 'Testing' })
-  .set('started', true);
+  .set('year', 2022);
 
 console.log(course);
 
-// Unlike a standard object in JavaScript, with Map we must use the get() method to access the values.
+// Unlike a standard object in JavaScript, with Map we must use the get() method to access the values
 console.log(course.get('React'));
 
-for (key of course.keys()) {
+// Unlike objects, Maps can iterate their elements in their insertion order
+course.forEach(item => console.log(item));
+
+for (let item of course) console.log(item);
+
+for (let entry of course.entries()) {
+  console.log(entry);
+}
+
+for (let key of course.keys()) {
   console.log(key);
 }
 
-for (value of course.values()) {
+for (let value of course.values()) {
   console.log(value);
-}
-
-for (item of course) {
-  console.log(item);
-}
-
-for (entry of course.entries()) {
-  console.log(entry);
 }
 
 // Weak references
@@ -85,13 +88,13 @@ for (entry of course.entries()) {
 // WeakSet is similar to Set, but it can only store objects, it is not enumerable, and there is no way to loop over the items contained within it because there is no list of current objects stored in the collection; they are weakly referenced and may be removed at any point.
 
 const animals = new WeakSet();
-const lion = { name: 'Ray' };
 const turtle = { name: 'Tin-tin' };
+let lion = { name: 'Ray' };
 
-animals.add(lion);
 animals.add(turtle);
+animals.add(lion);
 
-animals.delete(lion);
+lion = 'Ray';
 
 console.log(animals);
 console.log(animals.has(lion));
@@ -104,7 +107,7 @@ console.log(animals.has(turtle));
 let pets = new WeakMap();
 let dog = { name: 'Charlie' };
 
-pets.set(dog, 'bau');
+pets.set(dog, { size: 'medium' });
 
 dog = null; // isn't reachable (garbage collected)
 
@@ -246,7 +249,7 @@ const gangMap = new Map(Object.entries(gang));
 
 console.log(gangMap);
 
-for (const value of gangMap.values()) {
+for (let value of gangMap.values()) {
   console.log(value);
 }
 
@@ -385,8 +388,10 @@ console.log((song &&= 'Map Of Your Head'));
 
 cat.age ??= 5;
 
-console.log(cat);
-
-cat.age ?? (cat.age = 5.5);
+cat.age ?? (cat.age = null);
 
 console.log(cat.age);
+
+cat.breed ??= 'Scottish Fold';
+
+console.log(cat);
