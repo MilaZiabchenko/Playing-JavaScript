061dbe5d31e4d7ee393bf91ab9048e34d4405ac7
Mila Ziabchenko <mi.podgurska@gmail.com>
2022-07-19 14:28:50 +0300

    Move .js files to js/ folder and add minor changes to all files in Regular Expression

--- Regular Expressions/js/validation.js
+++ Regular Expressions/js/validation.js
@@ -1,23 +1,25 @@
 const inputs = document.querySelectorAll('input');
+const infoToHide = document.querySelector('h3');
 
 const patterns = {
   username: /^[a-z\d]{3,12}$/i,
   password: /^[\w@-]{8,20}$/,
-  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  email: /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
   telephone: /^\+380\d{9}$/,
   slug: /^[a-z\d-]{8,20}$/,
 };
 
-function validate(field, regex) {
+const validate = (field, regex) => {
   if (regex.test(field.value)) {
     field.className = 'valid';
   } else {
     field.className = 'invalid';
   }
-}
+};
 
 inputs.forEach(input => {
   input.addEventListener('keyup', e => {
     validate(e.target, patterns[e.target.attributes.name.value]);
+    infoToHide.classList.add('hidden');
   });
 });
